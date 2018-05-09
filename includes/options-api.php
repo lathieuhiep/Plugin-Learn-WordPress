b9164afca64f4dd22379dbbf3a6ee1c4e4df2c90
lathieuhiep <khacdiepkma90@gmail.com>
2018-05-09 14:52:45 +0700

    code option-api

--- includes/options-api.php
+++ includes/options-api.php
@@ -1,25 +1,18 @@
 <?php
 
-// add option mail
-//add_option( 'mailer_gmail_username', 'khacdiepkma90@gmail.com' );
-//add_option( 'mailer_gmail_password', 'lathieuhiep0383597629' );
+// Hàm bổ sung menu con vào một menu cha
+function add_submenu_options() {
 
-// Hàm get_option
-//echo get_option('mailer_gmail_username');
+    add_submenu_page( 'themes.php', 'Theme Options', 'Theme Options', 'manage_options', 'theme-options', 'access_menu_options' );
 
-// hàm delete_option
-//delete_option( 'mailer_gmail_username' );
-//delete_option( 'mailer_gmail_password' );
+}
 
-// Dùng lệnh var_dump thay vì echo vì lúc này là false
-//var_dump( get_option( 'mailer_gmail_username' ) );
+// Hàm xử lý khi click vào menu
+function access_menu_options() {
 
-// Hàm update_option
-//update_option( 'mailer_gmail_password', 'password@@@' );
-//
-//echo '<br/>';
-//
-//echo get_option('mailer_gmail_password');
+    require( 'templates/theme-option.php' );
 
-// Die để dễ nhìn kết quả
-//die;
+}
+
+// Thêm hành động hiển thị menu con vào Action admin_menu Hooks
+add_action( 'admin_menu', 'add_submenu_options' );
