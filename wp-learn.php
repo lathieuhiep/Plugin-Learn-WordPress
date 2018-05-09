de575383eda71c0f0eaf98c21e76163dbf7bffb9
lathieuhiep <khacdiepkma90@gmail.com>
2018-05-09 11:23:01 +0700

    create file php wp-learn

---
+++ wp-learn.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Learn WordPress
+Plugin URI: https://www.facebook.com/lathieuhiep
+Description: Plugin dùng để học WordPress.
+Version: 1.0.0
+Author: La Thiếu Hiệp
+Author URI: https://www.facebook.com/lathieuhiep
+License: GPLv2 or later
+Text Domain: wp-learn
+*/
